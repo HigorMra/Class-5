3291827be96d39a80832e278f265e6e2149ef8df
Higor Moreira <167116708+HigorMra@users.noreply.github.com>
2024-04-16 16:16:52 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+# comentário
+
+# nome = input("Digite seu nome: ")
+# idade = int(input(f"Qual a sua idade {nome}?"))
+
+# nascimento = 2024 - idade
+
+# print(f"{nome} você nasceu em {nascimento}")
+
+# calculadora
+
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+resultado = num1 + num2
+
+print(f"O resultado é: {resultado}")
